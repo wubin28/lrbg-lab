67432b28426a0848bd8a18c25b90c80b1e3fd04e
Bin Wu <wubin28@gmail.com>
2024-12-13 16:04:15 +0800

    feat: finished iteration 5 - A beep sound will be made each time button B is pressed to confirm a selection

--- ch03/mem-match/src/main.rs
+++ ch03/mem-match/src/main.rs
@@ -4,10 +4,16 @@
 
 use core::fmt::Write;
 use cortex_m_rt::entry;
+use embedded_hal::blocking::delay::DelayMs;
 use embedded_hal::digital::v2::InputPin;
 use microbit::board::Board;
 use microbit::display::blocking::Display;
+use microbit::hal::gpio::Level;
+use microbit::hal::prelude::*;
+use microbit::hal::pwm::{Channel, Pwm};
+use microbit::hal::time::Hertz;
 use microbit::hal::Timer;
+use microbit::pac::{PWM0, TIMER0};
 use panic_probe as _;
 use rtt_target::rtt_init;
 
@@ -133,6 +139,14 @@ enum GameState {
     ShowingPatterns,
 }
 
+fn make_beep(pwm: &mut Pwm<PWM0>, timer: &mut Timer<TIMER0>) {
+    pwm.set_period(Hertz(1000));
+    pwm.set_duty_on_common(pwm.max_duty() / 2);
+    pwm.enable(Channel::C0);
+    timer.delay_ms(100_u32);
+    pwm.disable(Channel::C0);
+}
+
 #[entry]
 fn main() -> ! {
     let mut channels = rtt_init! {
@@ -149,12 +163,30 @@ fn main() -> ! {
     let board = Board::take().unwrap();
     let mut display = Display::new(board.display_pins);
     let mut timer = Timer::new(board.TIMER0);
+
+    // Enable the speaker
+    board.pins.p0_02.into_push_pull_output(Level::High);
+
+    // Get the speaker output pin
+    let speaker_pin = board.speaker_pin.into_push_pull_output(Level::Low);
+
     let button_a = board.buttons.button_a;
     let button_b = board.buttons.button_b;
 
+    // Get P0_13 from the board and convert it to push-pull output
+    board
+        .pins
+        .p0_13
+        .into_push_pull_output(microbit::hal::gpio::Level::Low);
+
+    // Initialize the PWM (Pulse Width Modulation)
+    let mut pwm = Pwm::new(board.PWM0);
+    // Degrade the speaker pin to a general purpose pin and set it as a PWM output pin
+    pwm.set_output_pin(Channel::C0, speaker_pin.degrade());
+
     let mut game_state = GameState::ShowingSmiley;
     let mut display_buffer = [[0u8; 5]; 5];
-    let mut current_pattern = 0;
+    let mut current_pattern;
 
     let seed = timer.read();
     let mut rng = XorShiftRng::new(seed);
@@ -198,6 +230,7 @@ fn main() -> ! {
                     }
 
                     if current_button_b_state && !last_button_b_state {
+                        make_beep(&mut pwm, &mut timer);
                         writeln!(channel, "Selected pattern: {}", current_pattern).ok();
                         game_state = GameState::ShowingSmiley;
                         break;
